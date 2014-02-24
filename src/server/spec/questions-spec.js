8389f759ad6d7d6d40961e581f7b92b303df2809
DmitryBatalov <dmitryabat@gmail.com>
2014-02-24 08:34:04 +0000

    The tests on server side were added.

---
+++ src/server/spec/questions-spec.js
@@ -0,0 +1,10 @@
+
+// spec/questions-spec.js
+// var questions = require("../questions");
+ 
+describe("qustions >", function () {
+  it("it should be possible to save new question", function () {
+    
+    expect(true).toBe(false);
+  });
+});
