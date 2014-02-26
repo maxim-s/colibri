1e6484ab5050b233c036199b471511ef5d87558c
Maxim Salamatko <maxim.salamatko@gmail.com>
2014-02-26 08:29:42 +0000

    Question Controller was added.

---
+++ src/server/controllers/QuestionController.js
@@ -0,0 +1,25 @@
+/*
+*	Question controller
+*
+*/
+
+var QuestionController = module.exports = function () {
+	
+}
+
+QuestionController.prototype.list = function(req,res){
+	res.send({});
+}
+QuestionController.prototype.get = function(req,res){
+	res.send({});
+}
+QuestionController.prototype.update = function(req,res){
+	res.send({});
+}
+QuestionController.prototype.create = function(req,res){
+	res.send({});
+}
+QuestionController.prototype.remove = function(req,res){
+	res.send({});
+}
+
