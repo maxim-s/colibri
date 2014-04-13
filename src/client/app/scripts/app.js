48a90906afb20e31ddc32b96d9b70527863e9533
Maxim Salamatko <maxim.salamatko@gmail.com>
2014-04-13 09:26:03 +0000

    Added http auth interceptor

--- src/client/app/scripts/app.js
+++ src/client/app/scripts/app.js
@@ -1,31 +1,33 @@
 /*jshint unused: vars */
-define(['angular', 'controllers/main', 'controllers/questions', 'controllers/question-view', 'services/url-resolver']/*deps*/, function (angular, MainCtrl, QuestionsCtrl, QuestionViewCtrl, $url)/*invoke*/ {
-  'use strict';
+define(['angular', 'controllers/main', 'controllers/questions', 'controllers/question-view', 'services/url-resolver', 'services/auth']/*deps*/, function (angular, MainCtrl, QuestionsCtrl, QuestionViewCtrl, $url, AuthFactory)/*invoke*/ {
+    'use strict';
 
-  return angular.module('colibriApp', ['colibriApp.controllers.MainCtrl',
-'colibriApp.controllers.QuestionsCtrl',
-'colibriApp.controllers.QuestionViewCtrl',
-'colibriApp.services.UrlResolver',
-/*angJSDeps*/
-  'ngResource',
-  'ngRoute'
-])
-    .config(function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/main.html',
-          controller: 'MainCtrl'
-        })
-        .when('/Questions', {
-          templateUrl: 'views/questions.html',
-          controller: 'QuestionsCtrl'
-        })
-        .when('/question-view/:id', {
-          templateUrl: 'views/question-view.html',
-          controller: 'QuestionViewCtrl'
-        })
-        .otherwise({
-          redirectTo: '/'
+    return angular.module('colibriApp', ['colibriApp.controllers.MainCtrl',
+            'colibriApp.controllers.QuestionsCtrl',
+            'colibriApp.controllers.QuestionViewCtrl',
+            'colibriApp.services.UrlResolver',
+            'colibriApp.services.Auth',
+            /*angJSDeps*/
+            'ngResource',
+            'ngRoute'
+        ])
+        .config(function ($routeProvider, $httpProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'views/main.html',
+                    controller: 'MainCtrl'
+                })
+                .when('/Questions', {
+                    templateUrl: 'views/questions.html',
+                    controller: 'QuestionsCtrl'
+                })
+                .when('/question-view/:id', {
+                    templateUrl: 'views/question-view.html',
+                    controller: 'QuestionViewCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+            $httpProvider.interceptors.push('auth');
         });
-    });
 });
