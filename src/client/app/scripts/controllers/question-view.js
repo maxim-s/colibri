75cf4a40005cae55b6d90176b9b2c142b6bcff0f
Aliskerov_Dmitry <dmitry.aliskerov@fastdev.se>
2014-04-14 10:01:12 +0400

    added navigation in index.html, added question-view design

--- src/client/app/scripts/controllers/question-view.js
+++ src/client/app/scripts/controllers/question-view.js
@@ -3,7 +3,12 @@ define(['angular'], function (angular) {
 
     angular.module('colibriApp.controllers.QuestionViewCtrl', [])
         .controller('QuestionViewCtrl', function ($scope, $resource, $routeParams) {
-            $scope.questionResource = $resource('http://localhost:3000/questions/:id');
+            $scope.questionResource = $resource('http://192.168.206.136:3000/questions/:id');
             $scope.question = $scope.questionResource.get({id: $routeParams.id});
+            $scope.ratingClick = function ($answer, $value) { alert('\nAnswerObject = ' + $answer + "\nAnswerBody=" + $answer.body + '\nValue = ' + $value) };
+	    $scope.sendAnswer = function($text) { alert('QId = ' + $routeParams.id + '\nText = ' + $text) };
+	    $scope.addQuestionComment = function($text) { alert('QId = ' + $routeParams.id + '\nText = ' + $text) };
+	    $scope.addAnswerComment = function($answer, $text) { alert('\nAnswerObject = ' + $answer + "\nAnswerBody=" + $answer.body + '\nText = ' + $text) };
         });
+
 });
